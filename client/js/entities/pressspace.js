8e4ca0e4befa3417f910a4f0f180491e6fca4297
Alvin Lao <alvin.lao.is@gmail.com>
2016-02-16 12:01:30 -0800

    extract press space to entities

---
+++ client/js/entities/pressspace.js
@@ -0,0 +1,43 @@
+var spritesheets = require('../util/spritesheets.js');
+
+class PressSpace extends Phaser.Sprite {
+  constructor(game, x, y, callback, callbackContext) {
+    super(game, x, y);
+    game.add.existing(this);
+
+    this.callback = callback;
+    this.callbackContext = callbackContext;
+
+    // Animated space bar
+    this.spacebarSprite = game.add.sprite(x, y, spritesheets.spacebar.name);
+    this.spacebarSprite.anchor.setTo(0.5);
+    this.spacebarSprite.animations.add('tap', [0, 1], 2, true);
+    this.spacebarSprite.animations.play('tap');
+
+    // Listen for input
+    game.input.keyboard.addKeyCapture(Phaser.KeyCode.SPACEBAR);
+    this.spacebar = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+
+    this.spacebar.onDown.add(this.down, this, 0);
+    this.spacebar.onUp.add(this.up, this, 0);
+  }
+
+  down() {
+    this.spacebarSprite.animations.stop();
+    this.spacebarSprite.frame = 1;
+
+    // Unbind
+    this.spacebar.onDown.remove(this.down, this);
+  }
+
+  up() {
+    this.spacebarSprite.frame = 0;
+    this.spacebar.onUp.remove(this.up, this);
+
+    this.game.time.events.add(Phaser.Timer.HALF, function() {
+      this.callback.call(this.callbackContext);
+    }, this);
+  }
+}
+
+module.exports = PressSpace;
--- client/js/states/title.js
+++ client/js/states/title.js
@@ -1,6 +1,8 @@
 var spritesheets = require('../util/spritesheets.js');
 var config = require('../util/config.js');
 
+var PressSpace = require('../entities/pressspace.js');
+
 class TitleState extends Phaser.State {
   create() {
     super.create();
@@ -17,38 +19,11 @@ class TitleState extends Phaser.State {
     var title = game.add.text(x, y + titleYOffset, "Ape Escape", style);
     title.anchor.set(0.5);
 
-    
-    // Animated space bar
-    this.spacebarSprite = game.add.sprite(x, y + spaceYOffset, spritesheets.spacebar.name);
-    this.spacebarSprite.anchor.setTo(0.5);
-    this.spacebarSprite.animations.add('tap', [0, 1], 2, true);
-    this.spacebarSprite.animations.play('tap');
-
-    // Listen for input
-    game.input.keyboard.addKeyCapture(Phaser.KeyCode.SPACEBAR);
-    this.spacebar = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-
-    this.spacebar.onDown.add(this.down, this, 0);
-    this.spacebar.onUp.add(this.up, this, 0);
-  }
-
-  down() {
-    this.spacebarSprite.animations.stop();
-    this.spacebarSprite.frame = 1;
-
-    // Unbind
-    this.spacebar.onDown.remove(this.down, this);
-  }
-
-  up() {
-    this.spacebarSprite.frame = 0;
-    this.spacebar.onUp.remove(this.up, this);
-
-    this.game.time.events.add(Phaser.Timer.HALF, function() {
+    // Press Space
+    var pressspace = new PressSpace(game, x, y + spaceYOffset, function() {
       // TODO: Go to lobby
       //this.game.state.start('lobby');
       this.game.state.start('level');
-
     }, this);
   }
 }
